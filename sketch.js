b6028b9a8801a6bc4700ce50816b3d868ef17ea1
Tangelo34 <user@Air3.local>
2017-03-07 13:52:42 -0500

    Upload to master

---
+++ sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+createCanvas(400,400);//p1 Changes the width, p2 changes the height 
+}
+
+function draw() {
+background(mouseX ,mouseY ,14);// p1 changes the color (if we have three parameters they are red, blue, and green)
+rect(50,100,160,200);//p1 moves the rectangle on the x axis, p2 moves the rectangle on the y axis, p3 width, p4 height 
+}
